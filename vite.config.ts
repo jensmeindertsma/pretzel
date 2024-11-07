d341e465d3222ba6be212117804585e0f7c4e9be
Jens Meindertsma <mail@jensmeindertsma.com>
2024-11-07 16:20:22 +0100

    Enable unstable option

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
   plugins: [
     remix({
       future: {
+        unstable_optimizeDeps: true,
         v3_fetcherPersist: true,
         v3_lazyRouteDiscovery: true,
         v3_relativeSplatPath: true,
